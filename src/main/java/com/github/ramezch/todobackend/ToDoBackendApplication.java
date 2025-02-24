da2d32a4425cce08881764eabbd50f835f1a3a7b
Ramez Chreide <ramiz_chreide@outlook.com>
2025-02-24 14:19:40 +0100

    Initial commit

---
+++ src/main/java/com/github/ramezch/todobackend/ToDoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.ramezch.todobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoBackendApplication.class, args);
+	}
+
+}
