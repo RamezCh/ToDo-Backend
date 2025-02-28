2c298a030ca758466c276744a162c7860593ccc0
Ramez Chreide <ramiz_chreide@outlook.com>
2025-02-28 12:21:34 +0100

    add: Integration Tests for Add/Update

--- src/test/java/com/github/ramezch/todobackend/task/controllers/TaskIntegrationTest.java
+++ src/test/java/com/github/ramezch/todobackend/task/controllers/TaskIntegrationTest.java
@@ -1,6 +1,9 @@
 package com.github.ramezch.todobackend.task.controllers;
 
+import com.github.ramezch.todobackend.gpt.models.GPTAnswer;
+import com.github.ramezch.todobackend.gpt.services.OpenAIService;
 import com.github.ramezch.todobackend.task.models.Task;
+import com.github.ramezch.todobackend.task.models.TaskDTO;
 import com.github.ramezch.todobackend.task.models.TaskStatus;
 import com.github.ramezch.todobackend.task.repositories.TaskRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,8 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,11 +30,69 @@ class TaskIntegrationTest {
     @Autowired
     TaskRepository taskRepo;
 
+    @MockitoBean
+    private OpenAIService ai;
+
     Task newTask;
 
     @BeforeEach
     void setup() {
         newTask = new Task("1", "hi", TaskStatus.OPEN);
+    }
+
+    @Test
+    @DirtiesContext
+    void addTask_returnNewTask() throws Exception {
+        // GIVEN
+        TaskDTO taskDTO = new TaskDTO("eat apple", TaskStatus.OPEN);
+        GPTAnswer gptAnswer = new GPTAnswer(true, "eat apple");
+        when(ai.checkSpelling(taskDTO.description())).thenReturn(gptAnswer);
+
+        // WHEN
+        mvc.perform(post("/api/todo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                        {
+                            "description": "eat apple",
+                            "status": "OPEN"
+                        }
+                        """))
+                // THEN
+                .andExpect(status().isCreated())
+                .andExpect(content().json("""
+                        {
+                            "description": "eat apple",
+                            "status": "OPEN"
+                        }
+                        """));
+    }
+
+    @Test
+    @DirtiesContext
+    void updateTask_returnNewTask() throws Exception {
+        // GIVEN
+        taskRepo.save(newTask);
+        TaskDTO taskDTO = new TaskDTO("eat apple", TaskStatus.OPEN);
+        GPTAnswer gptAnswer = new GPTAnswer(true, "eat apple");
+        when(ai.checkSpelling(taskDTO.description())).thenReturn(gptAnswer);
+
+        // WHEN
+        mvc.perform(put("/api/todo/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                        {
+                            "description": "eat apple",
+                            "status": "OPEN"
+                        }
+                        """))
+                // THEN
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "description": "eat apple",
+                            "status": "OPEN"
+                        }
+                        """));
     }
 
     @Test
